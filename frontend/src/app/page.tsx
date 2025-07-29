b0bd443cb7668a3e5671d8f5e5082f5234f45cc7
Tariqul Islam <tariqul.isha@gmail.com>
2025-07-30 00:10:09 +0100

    use prettier

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,22 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/hello`)
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
-      .catch((err) => setMessage('Error fetching message: ' + err.message));
+      .catch((err) => setMessage("Error fetching message: " + err.message));
   }, []);
 
   return (
     <main>
       <h1>Frontend</h1>
       <p>Backend says: {message}</p>
+      <p>Test</p>
     </main>
   );
 }
